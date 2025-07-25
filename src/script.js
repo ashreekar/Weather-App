b0065afa8992b3d0961385fafcb19f53dc92c614
ashreekar <ashreekar767@gmail.com>
2025-07-25 13:35:20 +0530

    added upadtion of city feature

--- src/script.js
+++ src/script.js
@@ -13,7 +13,8 @@ let forecastShowingBlock=document.querySelector("#forecastShowingBlock");
 // Use below this to render once to reduce reflow and repaint
 let AppendWholeData=document.querySelector("#AppendWholeData");
 
-const renderCityAPIData=(data)=>{
+const renderCityAPIData=(data,timeNow)=>{
+
   cityDataSection.innerHTML="";
   cityDataSection.innerHTML=`<div class="col-start-1 col-end-2 row-start-1 row-end-3 h-[16vh] bg-blue-300 shadow-2xl rounded-md flex items-center flex-col justify-center">
             <h3 class="font-bold text-sm sm:text-2xl lg:text-2xl text-center">${data["name"]}</h3>
@@ -28,7 +29,7 @@ const renderCityAPIData=(data)=>{
         <div class="col-start-1 col-end-5 row-start-3 row-end-5 flex flex-row h-[16vh] gap-[10px] sm:gap-[30px] w-[100%] justify-evenly">
             <div class="bg-blue-300 shadow-2xl rounded-md w-[30%] sm:w-[25%] hidden items-center flex-col justify-center sm:flex">
                 <h4 class="font-medium text-sm sm:text-base text-gray-700">Time</h4>
-                <p class="font-bold text-base sm:text-2xl">${"12:30"}<span> AM</span></p>
+                <p class="font-bold text-base sm:text-2xl">${timeNow}</p>
             </div>
 
             <div class="bg-blue-300 shadow-2xl rounded-md w-[30%] sm:w-[25%] flex items-center flex-col justify-center">
@@ -43,7 +44,7 @@ const renderCityAPIData=(data)=>{
 
             <div class="bg-blue-300 shadow-2xl rounded-md w-[30%] sm:w-[25%] flex items-center flex-col justify-center">
                 <h4 class="font-medium text-sm sm:text-base text-gray-700">Altitude</h4>
-                <p class="font-bold text-base sm:text-2xl">${data["main"]["sea_level"]}<span class="text-gray-500"> meter</span></p>
+                <p class="font-bold text-base sm:text-2xl">${Math.round(44330*((1-(((data["main"]["grnd_level"])/(data["main"]["sea_level"]))**(1/5.255)))))}<span class="text-gray-500"> meter</span></p>
             </div>
         </div>
 
@@ -72,10 +73,25 @@ Failed to fetch data.`);
     }
 }
 
+// Function to get exact time of the city
 const getTimeNowInCity=(data)=>{
-  let timezone=data["timezone"];
-  let timeZoneDiff=timezone/3600;
-  console.log(timeZoneDiff);
+  // fetch timezone in seconds to hours
+  let timezone=Number(data["timezone"]);
+  
+  let timeNow=Number(data["dt"]);
+
+  let localTimeStamp=(timeNow+timezone)*1000;
+
+  const localDateInKoppa = new Date(localTimeStamp);
+
+  const formattedTime = localDateInKoppa.toLocaleTimeString('en-IN', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+    timeZone: 'UTC'
+  });
+
+ return formattedTime;
 }
 
 // Function to get searched city
@@ -85,7 +101,7 @@ const getSearchData= async (event)=>{
   let data=await getDataFromCityAPI(searchPlace);
   //console.log(data)
   let timeNow=getTimeNowInCity(data);
-  //renderCityAPIData(data);
+  renderCityAPIData(data,timeNow);
 }
 
 // function to get gps location
